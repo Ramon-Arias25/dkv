88c55624e5e91720cecedeb0220350cb8107b452
Ramon-Arias25 <reab987@gmail.com>
2023-05-01 20:18:31 -0300

    estilos de marketing y mercado

--- src/app/components/marketing/marketing.component.ts
+++ src/app/components/marketing/marketing.component.ts
@@ -8,22 +8,38 @@ import { Component } from '@angular/core';
 export class MarketingComponent {
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   handleHover(id: string) {
-    const siblings = document.querySelectorAll(`#${id}`);
-    siblings.forEach(sibling => {
-      (sibling as HTMLElement).style.backgroundSize = '200%';
-      (sibling as HTMLElement).style.transition  = 'background-size 0.3s ease-in-out';
+    document.querySelectorAll(`#${id}`).forEach((sibling) => {
+      (sibling as HTMLElement).style.backgroundSize = '220%';
+      (sibling as HTMLElement).style.transition ='background-size 0.3s ease-in-out';
+    });
+    
+    document.querySelectorAll(`#base-flecha-${id},#punta-flecha-${id}`).forEach((flechaSibling) => {
+      (flechaSibling as HTMLElement).style.borderTop ='solid 15px var(--unnamed-color-98a92a)';
+      (flechaSibling as HTMLElement).style.transition ='border-top 0.3s ease-out';
+    });
+
+    document.querySelectorAll(`#selector-punto-${id}`).forEach((selectorPuntoSibling) => {
+      (selectorPuntoSibling as HTMLElement).style.opacity ='1';
+      (selectorPuntoSibling as HTMLElement).style.transition ='opacity 0.3s ease-out';
     });
   }
 
   handleMouseLeave(id: string) {
-    const siblings = document.querySelectorAll(`#${id}`);
-    siblings.forEach(sibling => {
-      (sibling as HTMLElement).style.backgroundSize = '170%';
-      (sibling as HTMLElement).style.transition  = 'background-size 0.3s ease-in-out';
+
+    document.querySelectorAll(`#${id}`).forEach((sibling) => {
+      (sibling as HTMLElement).style.backgroundSize = '200%';
+      (sibling as HTMLElement).style.transition ='background-size 0.3s ease-in-out';
+    });
+
+    document.querySelectorAll(`#base-flecha-${id},#punta-flecha-${id}`).forEach((flechaSibling) => {
+      (flechaSibling as HTMLElement).style.borderTop = 'solid 15px var(--unnamed-color-43752b)';
+      (flechaSibling as HTMLElement).style.transition = 'border-top 0.3s ease-out';
+    });
+
+    document.querySelectorAll(`#selector-punto-${id}`).forEach((selectorPuntoSibling) => {
+      (selectorPuntoSibling as HTMLElement).style.opacity ='0';
+      (selectorPuntoSibling as HTMLElement).style.transition ='opacity 0.3s ease-out';
     });
   }
 }
