895b09c9792f1da0158c2bee1533138a2bf5f8f4
Ramon-Arias25 <reab987@gmail.com>
2023-05-08 20:47:15 -0300

    nuevos link de iFrame

--- src/app/components/i-frame-template/i-frame-template.component.ts
+++ src/app/components/i-frame-template/i-frame-template.component.ts
@@ -18,14 +18,14 @@ export class IFrameTemplateComponent{
   /*2  */ 'https://dkvseguros.inteliens.com/',//ARGUMENTARIOS
   /*3  */ 'https://www.plimsoll.es/',//INFORME SECTORIAL CORREDURIAS
   /*4  */ '',//NOTICIAS SECTORIALES
-  /*5  */ 'https://app.powerbi.com/reportEmbed?reportId=ffcdecfa-13c1-4c05-8428-670c04682ed1&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db%22',//por que llega
-  /*6  */ 'https://app.powerbi.com/reportEmbed?reportId=90f9e1a2-1d60-48dc-9748-51d65704eed9&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63d',//como es
-  /*7  */ 'https://app.powerbi.com/reportEmbed?reportId=f7507751-6712-430c-bb44-02470d31bc6f&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63d',//como actua
-  /*8  */ 'https://app.powerbi.com/reportEmbed?reportId=89f49aa4-8d37-48e5-a2da-b6016e252a0b&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63d',//donde vive
-  /*9  */ 'https://app.powerbi.com/reportEmbed?reportId=a549b35c-bd59-4c44-98c8-8830dbe43e5c&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63d',//que le motiva
-  /*10 */ 'https://app.powerbi.com/reportEmbed?reportId=b5955202-f67d-4004-9d52-319edaab4904&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca6',// por que se va
-  /*11 */ 'https://app.powerbi.com/reportEmbed?reportId=1be6869d-1259-4ce0-81ff-58a07bca0fd0&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//cuadro de mando
-  /*12 */ 'https://app.powerbi.com/reportEmbed?reportId=25b9ebf8-8293-40f9-b404-1c21a3ab606d&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//estudio de bajas
+  /*5  */ 'https://app.powerbi.com/reportEmbed?reportId=ffcdecfa-13c1-4c05-8428-670c04682ed1&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//por que llega
+  /*6  */ 'https://app.powerbi.com/reportEmbed?reportId=90f9e1a2-1d60-48dc-9748-51d65704eed9&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//como es
+  /*7  */ 'https://app.powerbi.com/reportEmbed?reportId=f7507751-6712-430c-bb44-02470d31bc6f&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//como actua
+  /*8  */ 'https://app.powerbi.com/reportEmbed?reportId=89f49aa4-8d37-48e5-a2da-b6016e252a0b&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//donde vive
+  /*9  */ 'https://app.powerbi.com/reportEmbed?reportId=a549b35c-bd59-4c44-98c8-8830dbe43e5c&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//que le motiva
+  /*10 */ 'https://app.powerbi.com/reportEmbed?reportId=25b9ebf8-8293-40f9-b404-1c21a3ab606d&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//por que se va
+  /*11 */ 'https://app.powerbi.com/reportEmbed?reportId=b5955202-f67d-4004-9d52-319edaab4904&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//cuadro de mando
+  /*12 */ 'https://app.powerbi.com/reportEmbed?reportId=d3f496c2-9db7-4f53-b688-bc79b913d9bb&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//estudio de bajas
   /*13 */ 'https://app.powerbi.com/reportEmbed?reportId=1be6869d-1259-4ce0-81ff-58a07bca0fd0&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//campanias de marketing
   /*14 */ 'https://app.powerbi.com/reportEmbed?reportId=d30a1849-2bf2-478b-bd9f-e487fce301a0&autoAuth=true&ctid=38fd77bb-cf74-4d5c-9b52-df4f93ca63db',//dimensionamiento acciones 
   /*15 */ 'https://informa.inatlas.com/#/home'//geomarketing empresas
